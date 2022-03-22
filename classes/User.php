172530c80b55c9e4d148ddfc9e7d189dee6431fa
CharlotteJeuris <charlottejeuris@hotmail.com>
2022-03-22 18:45:02 +0100

    classes aanmaken

---
+++ classes/User.php
@@ -0,0 +1,148 @@
+<?php
+    class User{
+        private $id;
+        private $firstname;
+        private $lastname;
+        private $email;
+        private $password;
+
+        /**
+         * Get the value of id
+         */ 
+        public function getId()
+        {
+                return $this->id;
+        }
+
+        /**
+         * Set the value of id
+         *
+         * @return  self
+         */ 
+        public function setId($id)
+        {
+                $this->id = $id;
+
+                return $this;
+        }
+
+        
+        /**
+         * Get the value of firstname
+         */ 
+        public function getFirstname()
+        {
+                return $this->firstname;
+        }
+
+        /**
+         * Set the value of firstname
+         *
+         * @return  self
+         */ 
+        public function setFirstname($firstname)
+        {
+                $this->firstname = $firstname;
+
+                return $this;
+        }
+
+
+
+        /**
+         * Get the value of lastname
+         */ 
+        public function getLastname()
+        {
+                return $this->lastname;
+        }
+
+        /**
+         * Set the value of lastname
+         *
+         * @return  self
+         */ 
+        public function setLastname($lastname)
+        {
+                $this->lastname = $lastname;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of email
+         */ 
+        public function getEmail()
+        {
+                return $this->email;
+        }
+
+        /**
+         * Set the value of email
+         *
+         * @return  self
+         */ 
+        public function setEmail($email)
+        {
+                $this->email = $email;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of password
+         */ 
+        public function getPassword()
+        {
+                return $this->password;
+        }
+
+        /**
+         * Set the value of password
+         *
+         * @return  self
+         */ 
+        public function setPassword($password)
+        {
+                $this->password = $password;
+
+                return $this;
+        }
+    }
+
+
+
+    function canLogin($email, $password)
+    {
+        //db connectie
+        $conn = Db::getConnection();
+
+        //email zoeken in db
+        $statement = $conn->prepare('select * from users where email = :email');
+        $statement->bindParam(':email', $email);
+        $statement->execute();
+        $user = $statement->fetch(PDO::FETCH_ASSOC);
+
+        //passwoorden komen overeen?
+        if (password_verify($password, $user['password'])) {
+            //ja -> naar index
+            //echo "joepie de poepie!!!!";
+            return $user;
+        } else {
+            //nee -> error
+            //echo "jammer joh";
+            return false;
+        }
+    }
+
+    function doLogin($user)
+    {
+        session_start();
+        $_SESSION['email'] = $user['email'];
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['firstname'] = $user['firstname'];
+        // var_dump($user);
+        header('location: homepage.php');
+    }
+
+?>
--- login.php
+++ login.php
@@ -1,14 +1,69 @@
 <?php
 
-?><!DOCTYPE html>
+include_once(__DIR__ . "/classes/User.php");
+
+if (!empty($_POST)) {
+  //email en password opvragen
+  $email = htmlspecialchars($_POST['email']);
+  $password = $_POST['password'];
+  $user = $_POST['user'];
+}
+?>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>spacesse</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Login Spacesse</title>
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"> !-->
+  <!-- CSS Files -->
+  <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    
+<!--<?php //include_once(__DIR__ . "/includes/header.inc.php"); ?> !-->
+<img class="logo1" src="./images/buddy-logo.svg">
+  <div class="container">
+
+    <form class="form" method="post" action="">
+
+      <div class="header">
+        
+        <?php if (isset($error)) : ?>
+          <div class="form alert-danger">
+            <p>
+              Sorry, we can't log you in with that email address and password. Can you try again?
+            </p>
+          </div>
+        <?php endif; ?>
+        <h4>Login</h4>
+
+      </div>
+
+      <div class="login-body">
+
+        <div class="form-group">
+          <input type="text" name="email" id="email" class="form-control" placeholder="Email...">
+        </div>
+
+        <div class="form-group">
+          <input type="password" id="password" name="password" class="form-control" placeholder="Password...">
+        </div>
+
+      </div>
+
+
+      <input class="btn btn-primary" type="submit" value="Sign in" class="submit">
+
+    </form>
+
+    <a href="register.php">Not an account yet? Sign up here!</a>
+
+  </div>
 </body>
+
 </html>
