87886ce7a5f02025410ca9fd3aea3aeb1efabd1a
FaheerHakim <r0820158@student.thomasmore.be>
2022-04-02 13:30:41 +0200

    title changed

--- signup.php
+++ signup.php
@@ -57,7 +57,7 @@ if (!empty($_POST)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sign Up Form -Test-</title>
+    <title>Sign Up</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 
