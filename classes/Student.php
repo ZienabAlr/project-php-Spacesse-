2c6b7c81a31bd1c49a65aae213ca28c0efa2635d
ZienabAlr <r0784494@student.thomasmore.be>
2022-04-16 03:02:29 +0200

    login & student aangepast

--- classes/Student.php
+++ classes/Student.php
@@ -4,10 +4,20 @@ class Student extends User{
 
     public static function inlogedStudents(){
         $conn=DB::getConnection();
-        $statement = $conn->prepare("select email, password from userSignup");
+        // $statement = $conn->prepare("select email, password from userSignup");
+        $statement = $conn->prepare("select email from userSignup");
+        $statement->bindValue("email", $this->email);
         $statement->execute();
-        return $statement->fetchAll(PDO::FETCH_ASSOC);
+        // return $statement->fetchAll(PDO::FETCH_ASSOC);
+        $passWord= $statement->fetchAll(PDO::FETCH_ASSOC);
+        $hash= $passWord['password'];
 
+		if(password_verify($password, $hash) ){
+			return true; 
+		}else{
+			return false;
+		}
+       
     }
     
 }
--- login.php
+++ login.php
@@ -3,85 +3,20 @@
 include_once("bootstrap.php");
 
 if (!empty($_POST)) {
-  if (){
-    
-  }
-  //email en password opvragen
-  /*$email = htmlspecialchars($_POST['email']);
-  $password = $_POST['password'];
-  $user = $_POST['user'];*/
+ $user= new Student ();  
+ $user->setFirstname($_POST['email']);
+ $user->setLastname($_POST['password']);
+ $user::inlogedStudents();
 
-  /*try {
-    $user = new Student;
-    $user->setEmail($_POST['email']);
-    
+ // zie hoe je hier session kunt opstarten
+ /*if ($user::inlogedStudents()){
+   session_start();
+   $_SESSION 
+    header("Location:index.php"); 
+    die ();
   }*/
 }
-?>
-<!-- <!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Login Spacesse</title>
-
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
-  <!-- CSS Files -->
-  <!-- <link rel="stylesheet" href="css/style.css"> 
-</head>
-
-<body>-->
-<!--<?php //include_once(__DIR__ . "/includes/header.inc.php"); ?> !-->
-
-
-  <!-- <div class="container"> 
-
-    <form class="form" method="post" action="">
-
-      <h4>Login</h4>-->
-
-        <!-- Error -->
-        <!-- <div class="header">
-        
-          <?php //if (isset($error)) : ?>
-            <div>
-              <p>
-                Sorry, we can't log you in with that email address and password. Can you try again?
-              </p>
-            </div>
-          <?php //endif; ?>
-
-        </div>
-
-      <div>
-
-        <div class="form-group"> -->
-          <!-- <input type="text" name="email" id="email" class="form-control" placeholder="Email..."> - placeholder: Een Placeholder is een tijdelijk type content dat later vervangen zal worden met de definitieve media, dit is dus enkel zichtbaar indien de officiele tekst nog niet beschikbaar is -charlotte - 
-        </div>
-
-        <div class="form-group">
-          <input type="password" id="password" name="password" placeholder="Password...">
-        </div>
-
-      </div>
-
-
-      <input class="" type="submit" value="Login" class="submit">
-
-      <a href="register.php">Not an account yet? Sign up here!</a>
-    
-    </form>
-
-    
-
-  </div>
-</body>
-
-</html>-->
-
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html lang="en">
 
 <head>
