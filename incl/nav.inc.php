10fbba1eb0ab1f939a9369bd8307b56fe351fe22
ZienabAlr <r0784494@student.thomasmore.be>
2022-05-29 22:54:55 +0200

    final

--- incl/nav.inc.php
+++ incl/nav.inc.php
@@ -4,13 +4,17 @@
   // $_SESSION['user'];
 ?>
 <nav class="navbar">
-    <!-- <a href="index.php" class="logo">IMDflix</a> -->
-    <a href="index.php">Home</a>
-    <a href="mylist.php">My List</a>
+    <a href="index.php" class="logo"><img src="images/Spacesse.png" alt="spacesse"></a>
     
-    <form action="" method="get">
+    <div class="links">
+        <a href="logout.php" class="link navbar__logout">Logout</a>
+        <a href="#" class ="profile link"><img src="https://www.edarabia.com/wp-content/uploads/2015/11/7-ways-to-become-the-person-everyone-respects.jpg" alt="profile">Hi <?php /*echo $username */?> </a>
+        <a href="index.php" class= "link">Home</a>
+    <!-- <a href="mylist.php">My List</a> -->
+    </div>
+   <!-- <form action="" method="get"> 
       <input type="text" name="search">
-    </form>
+    </form>-->
     
-    <a href="logout.php" class="navbar__logout">Hi <?php /*echo $username */?>, logout?</a>
+    <!-- <a href="logout.php" class="links navbar__logout">Hi <?php /*echo $username */?>, logout?</a> -->
 </nav>
