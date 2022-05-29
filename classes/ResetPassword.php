a8a0b102bdf4558f6b5a5ef8abb6c257a9aa3099
ZienabAlr <r0784494@student.thomasmore.be>
2022-05-29 22:46:34 +0200

    final save

--- classes/ResetPassword.php
+++ classes/ResetPassword.php
@@ -106,65 +106,6 @@
 
     public function newPassword(){
 
-       /* include_once("bootstrap.php");
-
-        $conn = Db::getConnection();
-        session_start();
-        $error="";
-        if(isset($_GET['email']) && isset($_GET['token'])){
-            $_SESSION['email'] = $_GET['email'];
-            $token = $_GET['token'];
-
-         $curDate = date("Y-m-d H:i:s");
-        // Check in de database of de link correct is
-        $stat = $conn ->prepare("SELECT * FROM reset_password WHERE email = ?");
-        $stat->execute([$_SESSION['email']]);
-        $from_reset = $stat->fetch();
-        $expireDate= $from_reset['expDate'];
-
-            if ( $expireDate >= $curDate){
-                $valid = true; 
-                
-            }else{
-            
-                $error .= "<h2>Link Expired</h2>
-                <p>The link is expired. You are trying to use the expired link which 
-                as valid only 24 hours (1 days after request).<br /><br /></p>";
-            }if($error!=""){
-                echo "<div class='error'>".$error."</div><br />";
-                }			
-        }*/
-        
-        /*if(isset($_POST['reset'])){
-        $newPassword = $_POST['new_pass'];
-        $confirmPassword = $_POST['new_pass_conf'];
-
-        if($newPassword == $confirmPassword){
-
-            $hashed_password = password_hash($newPassword, PASSWORD_DEFAULT);
-        
-        } else {
-            $error = 'Passwords do not match!';
-        }
-        
-        $email = $_SESSION['email'];
-
-        // Update password
-        if(empty($error)){
-            $statement = $conn->prepare("UPDATE test SET password = ? WHERE email = ?");
-            $statement->execute([$hashed_password, $email]);
-
-            $msg = 'Successfully updated your password! <a class="btn btn-success" href="http://localhost:8888/test 2/login.php"> >>Log In</a>';
-
-            session_destroy();
-            //   var_dump($statement($hashed_password, $email));
-        }
-
-         }
-
-
-    }*/
-
         $updatedPassword= new ResetPassword;
         $newPassword =  $updatedPassword->getNewPassword();
         $confirmPassword = $updatedPassword->getConfPassword();
