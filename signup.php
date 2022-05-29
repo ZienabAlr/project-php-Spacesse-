b5bad24bdd98920f07011dceb5f88dea1698d9ab
FaheerHakim <r0820158@student.thomasmore.be>
2022-05-29 21:45:36 +0200

    JOOORIS I AM DEAD Faheeer

--- signup.php
+++ signup.php
@@ -50,10 +50,10 @@ if (!empty($_POST)) {
         $error = "Email is not valid";
     }
 
-    /*if (strlen($_POST["password"]) < 6) {
+    if (strlen($_POST["password"]) < 6) {
         $error = "Invalid password. Password must be at least 6 characters</redtext>";
         //$error = "Invalid password. Password must be at least 6 characters</redtext>";
-    }*/
+    }
 }
 
 ?>
@@ -80,22 +80,7 @@ if (!empty($_POST)) {
                     </p>
                 </div>
             <?php endif; ?>
-            <!--<script>
-function showHint(str) {
-  if (str.length == 0) { 
-    document.getElementById("txtHint").innerHTML = "";
-    return;
-  }
-  let xhttp = new XMLHttpRequest();
-  xhttp.onload = function() {
-    document.getElementById("txtHint").innerHTML =
-    this.responseText;
-  }
-  xhttp.open("GET", "gethint.php?q="+str);
-  xhttp.send();   
-}
-</script>
-<p>Suggestions: <span id="txtHint"></span></p>-->
+
             <div id="inputContainer">
                 
                 <input type="text" class="input" name="username" placeholder="a" onkeyup="showHint(this.value)">
