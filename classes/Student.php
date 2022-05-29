2ff3d930cb70571c0ea5bd6579e8fa6cb15a0b7f
FaheerHakim <r0820158@student.thomasmore.be>
2022-05-29 21:28:56 +0200

    okay

--- classes/Student.php
+++ classes/Student.php
@@ -9,5 +9,42 @@ class Student extends User{
         return $statement->fetchAll(PDO::FETCH_ASSOC);
 
     }
+    public function is_member($email) {
+        $conn = new PDO("mysql:host=localhost;dbname=Website", "root", "root");
+        $statement = $conn->prepare("SELECT * FROM user WHERE email='$email'");
+        $statement->execute();
+        $response = $statement->fetch(PDO::FETCH_ASSOC);
+
+        $results = (is_array($response) && count($response) > 0);
+        return $results;
+    }
+
+    public function can_signup($username, $email, $password)
+    {
+        try {
+            $conn = new PDO("mysql:host=localhost;dbname=Website", "root", "root");
+            $statement = $conn->prepare("INSERT INTO user (username, email, password) VALUES (:username, :email, :password)");
+            $statement->bindValue("username", $username);
+            $statement->bindValue("email", $email);
+            $options = [
+                'cost' => 12,
+            ];
+            $hash = password_hash($password, PASSWORD_DEFAULT,  $options);
+            $statement->bindValue("password", $hash);
+            return  $statement->execute();
+        } catch (Throwable $e) {
+
+            echo $e->getMessage();
+        }
+    }
+    public static function get_student_info($email)
+    {
+        $conn = new PDO("mysql:host=localhost;dbname=Website", "root", "root");
+        $statement = $conn->prepare("SELECT * FROM user WHERE email='$email'");
+        $statement->execute();
+        $user = $statement->fetch(PDO::FETCH_ASSOC);
+
+        return $user;
+    }
     
 }
