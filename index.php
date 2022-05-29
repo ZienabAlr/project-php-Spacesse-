fe4b4c5489ebcadfa1c0a63309c7e6c20d84fe88
ZienabAlr <r0784494@student.thomasmore.be>
2022-05-29 15:51:59 +0200

    index cleaned

--- index.php
+++ index.php
@@ -42,9 +42,7 @@ $posts = Post::show_post();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>home page</title>
-    <!-- <link rel="stylesheet" href="css/home.css"> -->
 	<link rel="stylesheet" href="css/main.css">
-	<!-- <script type="text/javascript"></script> -->
 
 </head>
 <body>
